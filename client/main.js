ad62fd94ac5f601997de0285d26073df8bd8c638
spirinvladimir <spirin.vladimir@gmail.com>
2015-04-25 13:51:00 +0300

    App

--- client/main.js
+++ client/main.js
@@ -2,11 +2,21 @@
 var domready = require('domready'),
     Map = require('./map'),
     map,
-    search = require('./search');//,
+    Search = require('./search');//,
     //io = require('socket.io-client')('http://localhost:1337');
 
 domready(function () {
     'use strict';
-    map = new Map();//io/);
-    //search();
+    var App = React.createClass({
+        render: function() {
+            return (
+                React.createElement(Map, null),
+                React.createElement(Search, null)
+            );
+        }
+    });
+    React.render(
+        React.createElement(App, null),
+        document.body
+    );
 });
--- client/map.js
+++ client/map.js
@@ -3,30 +3,27 @@ var React = require('react'),
     ReactGmaps = require('react-gmaps'),
     { Gmaps, Marker } = ReactGmaps;
     
-module.exports = function (io) {
-    'use strict';
-    
-    var coords = {
-        lat: 51.5258541,
-        lng: -0.08040660000006028 
-    }
-    
-    var App = React.createClass({
-        
+module.exports = React.createClass({
+        getInitialState: function () {
+            return {
+                lat: 51.5258541,
+                lng: -0.08040660000006028
+            };
+        },
         render() {
             return (
                 <Gmaps 
                 ref='Gmaps'
                 width={'100%'}
                 height={'100%'}
-                lat={coords.lat} 
-                lng={coords.lng} 
+                lat={this.state.coords.lat}
+                lng={this.state.coords.lng}
                 zoom={12} 
                 onMapCreated={this.onMapCreated}
                 onClick={this.onClick}>
                 <Marker 
-                lat={coords.lat} 
-                lng={coords.lng} />
+                lat={this.state.coords.lat}
+                lng={this.state.coords.lng} />
                 </Gmaps>
             );
         },
@@ -41,8 +38,4 @@ module.exports = function (io) {
         onClick() {
             console.log('onClick');
         }
-        
-    });
-
-    React.render(<App />, document.body);
-};
+});
